7654c2ab820840f485cc3d14c7ef3ad57cf40736
fabriciobrito01 <fabriciodbe@gmail.com>
2026-06-15 02:03:47 -0300

    cria packages para activities e fragments

--- android/app/src/main/java/com/example/lebronlegacyquiz/activities/HistoricoActivity.kt
+++ android/app/src/main/java/com/example/lebronlegacyquiz/activities/HistoricoActivity.kt
@@ -1,16 +1,17 @@
-package com.example.lebronlegacyquiz
+package com.example.lebronlegacyquiz.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lebronlegacyquiz.R
 
-class QuizActivity : AppCompatActivity() {
+class HistoricoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_quiz)
+        setContentView(R.layout.activity_historico)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- android/app/src/main/java/com/example/lebronlegacyquiz/activities/QuizActivity.kt
+++ android/app/src/main/java/com/example/lebronlegacyquiz/activities/QuizActivity.kt
@@ -1,16 +1,17 @@
-package com.example.lebronlegacyquiz
+package com.example.lebronlegacyquiz.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lebronlegacyquiz.R
 
-class HistoricoActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_historico2)
+        setContentView(R.layout.activity_quiz)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
