6d53ef21060347203945d4f7ed8b14334deca8bf
fabriciobrito01 <fabriciodbe@gmail.com>
2026-06-15 03:18:14 -0300

    atualiza home do app

--- android/app/src/main/java/com/example/lebronlegacyquiz/activities/HomeActivity.kt
+++ android/app/src/main/java/com/example/lebronlegacyquiz/activities/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lebronlegacyquiz.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.lebronlegacyquiz.R
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +19,13 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnComecar = findViewById<Button>(R.id.btnComecar)
+
+        btnComecar.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
