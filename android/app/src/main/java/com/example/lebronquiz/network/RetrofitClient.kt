96661ad35fc4a180df309f31bed9c990d54dbbf9
fabriciobrito01 <fabriciodbe@gmail.com>
2026-06-15 04:39:18 -0300

    atualiza porta retrofit

--- android/app/src/main/java/com/example/lebronquiz/network/RetrofitClient.kt
+++ android/app/src/main/java/com/example/lebronquiz/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/" // Default for Android Emulator to localhost
+    private const val BASE_URL = "http://10.0.2.2:8000/" // Default for Android Emulator to localhost
 
     val instance: LeBronApiService by lazy {
         val retrofit = Retrofit.Builder()
